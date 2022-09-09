abcb00baa8fd6a349526941896e0a5ba847beff2
Barka Park <you@example.com>
2022-09-09 09:34:55 +0200

    Eerste commit!

---
+++ helloPython/person.py
@@ -0,0 +1,9 @@
+print ('person.py')
+
+print ('----------------------------------------------------')
+naam = input ('wat is je naam')
+adres = input ('wat is je adres')
+postcode =  input ('wat is je postcode')
+woonplaats = input ('wat is je woonplaats')
+print ('----------------------------------------------------')
+
