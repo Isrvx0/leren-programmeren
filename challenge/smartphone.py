3dbf887ff77cd5a5f344126e8a71039e683dcaf6
Barka Park <you@example.com>
2022-09-22 20:50:59 +0200

    Eerste commit!

--- challenge/smartphone.py
+++ challenge/smartphone.py
@@ -1,36 +1,72 @@
-# smartphone challlenge 
 print ("keuzprogramma")
-()
-()
+
 samsung = int (input ('Hoe duur de samsung telefoon is?  '))
 iphone = int (input ('Hoe duur de Iphone telefoon is?  '))
+zenFone= int (input ('Hoe duur de Asus Zenfone telefoon is?  '))
 
-# duurste mobiel bepalen
-if samsung > iphone:
-     print ('Samsung is duurder dan de Iphone')
-     ()
-     ()
-     print (f'de samsung is het duurste, de telefoon kost {samsung} euro ')
-     print (f"de iphone is het goedkoopste, de telefoon kost {iphone} euro")
-     mingetal = iphone 
-     maxgetal = samsung
-     verschil= maxgetal - mingetal
-     advies= print (f'"het advies is dus de Iphone te kopen. Deze is namelijk {verschil} goedkoper dan de samung"')
-        
-elif iphone > samsung:
-    print ('iphone is duurder dan de samsung')
-    ()
-    ()
-    print (f"de iphone is het duurste, de telefoon kost {iphone} euro")
-    print (f'de samsung is het goedkoopste, de telefoon kost {samsung} euro ')
-    mingetal = samsung
-    maxgetal = iphone
-    verschil= maxgetal - mingetal
-    if verschil <= 50:
-        print (f'"het advies is dus de Iphone te kopen. Deze is namelijk {verschil} goedkoper dan de samung"')
-    else:
-        advies= print (f'"het advies is dus de samsung te kopen. Deze is namelijk {verschil} goedkoper dan de Iphone"') 
+maxgetal= max(samsung , iphone , zenFone)
+mingetal= min(samsung , iphone , zenFone)
 
-else: 
-    iphone == samsung
-    print ('de twee telefonen zijn even duur. het advies is de Iphone te kopen, want je hebt een voorkeur voor')
+def middle(samsung, iphone, zenFone) : 
+    return min(max(samsung,iphone),max(iphone,zenFone),max(samsung,zenFone))
+
+# verschil
+verschil= iphone - samsung #if iphone niet duurder dan 50 euro van samsung
+verschil1= samsung - zenFone #if zenfone 100 euro goedkoper dan samsung
+verschil2= iphone - zenFone #if zenfone 100 euro goedkoper dan Iphone
+
+#duurste
+()
+if iphone == maxgetal:
+     duurste= print ('Iphone is duurder dan de samsung en de ZenFone')
+     print (f"de iphone is dus het duurste, de telefoon kost {iphone} euro")
+elif samsung == maxgetal:
+     duurste= print ('samsung is duurder dan de Iphone en de ZenFone')
+     print (f'de samsung is dus het duurste, de telefoon kost {samsung} euro ')
+else:
+     duurste= print ('ZenFone is duurder dan de Iphone en de samsung ')
+     print (f' deZenFone is dus het duurste, de telefoon kost {zenFone} euro')
+
+# goedkoopste  
+()   
+if iphone == mingetal:
+     goedkoopste= print ('Iphone is goedkoopste dan de samsung en de ZenFone')
+     print (f"de iphone is dus het goedkoopste, de telefoon kost {iphone} euro")
+     
+elif samsung == mingetal:
+     goedkoopste= print ('samsung is goedkoopste dan de Iphone en de ZenFone') 
+     print (f'de samsung is dus het goedkoopste, de telefoon kost {samsung} euro ')
+else:
+     goedkoopste= print ('ZenFone is goedkoopste dan de Iphone en de samsung ')
+     print (f'de ZenFone is dus het goedkoopste, de telefoon kost {zenFone} euro')
+#gelijk
+()
+if iphone == samsung and iphone == zenFone and samsung == zenFone:
+     gelijk= print ('prijzen zijn gelijk')
+elif samsung == iphone:
+     gelijk= print ('samsung en iphone zijn gelijk')
+elif samsung == zenFone:
+     gelijk= print ('samsung en zenFone zijn gelijk')
+elif iphone == zenFone:
+     gelijk= print ('ZenFone en Iphone zijn gelijk')
+
+#tussen
+()
+if iphone == middle(samsung, iphone, zenFone):
+     print (f"de iphone zit er tussen met {iphone} euro")
+elif samsung == middle(samsung, iphone, zenFone):
+     print (f'de samsung zit er tussen met {samsung} euro ')
+else:
+     print (f' de ZenFone zit er tussen met {zenFone} euro')
+
+#advies
+if samsung > 900 or iphone > 900 or zenFone > 900:
+     print ("het advies is dus geen telefoon te kopen, ze zijn te duur!")
+elif verschil1 >= 100 and verschil2 >= 100:
+     print (f'het advies is dus om ZenFone te kopen. Deze is namelijk {verschil1} goedkoper dan samsung en {verschil2} goedkoper dan Iphone ')
+elif verschil >= 50:
+     print (f'"het advies is dus de Iphone te kopen. Deze is namelijk {verschil} goedkoper dan de samsung en {verschil2} goedkoper dan de Zenphone!"')
+elif iphone == mingetal:
+     print (f'"het advies is dus de iphone te kopen. Deze is namelijk {verschil} goedkoper dan de samsung en {verschil2} goedkoper dan de zenPhone"') 
+elif samsung == mingetal:     
+     print (f'"het advies is dus de samsung te kopen. Deze is namelijk {verschil} goedkoper dan de Iphone en {verschil1} goedkoper dan de zenPhone"') 
