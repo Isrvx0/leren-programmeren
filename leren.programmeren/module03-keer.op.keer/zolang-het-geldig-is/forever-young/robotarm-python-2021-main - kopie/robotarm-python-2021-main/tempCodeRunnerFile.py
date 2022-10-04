835dce5364340cb644b1663902bb6c69e2dfb850
Barka Park <you@example.com>
2022-10-04 11:06:39 +0200

    Eerste commit!

--- leren.programmeren/module03-keer.op.keer/zolang-het-geldig-is/forever-young/robotarm-python-2021-main - kopie/robotarm-python-2021-main/tempCodeRunnerFile.py
+++ leren.programmeren/module03-keer.op.keer/zolang-het-geldig-is/forever-young/robotarm-python-2021-main - kopie/robotarm-python-2021-main/tempCodeRunnerFile.py
@@ -1,1 +1,12 @@
-robotArm.moveRight()
+for i in range (1,10):
+    robotArm.grab()
+    color = robotArm.scan()
+    print (color)
+    if color == 'white':
+        robotArm.grab()
+        robotArm.moveRight()
+        robotArm.drop()
+        robotArm.moveRight()
+    else:
+        robotArm.drop()
+        robotArm.moveRight()
