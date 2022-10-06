7c15a374f54f2686e1c691a9d8608832410eb63b
Barka Park <you@example.com>
2022-10-06 10:06:36 +0200

    Opdracht 11 verbetert

--- leren.programmeren/module03-keer.op.keer/zolang-het-geldig-is/forever-young/robotarm-python-2021-main - kopie/robotarm-python-2021-main/example - kopie11.py
+++ leren.programmeren/module03-keer.op.keer/zolang-het-geldig-is/forever-young/robotarm-python-2021-main - kopie/robotarm-python-2021-main/example - kopie11.py
@@ -1,7 +1,7 @@
 from RobotArm import RobotArm
 robotArm = RobotArm('exercise 11')
 # van recht naar links
-for moveright in range (10):
+for moveright in range (9):
     robotArm.moveRight()
 
 for i in range (1,10):
