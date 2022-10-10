d3cee3bca96e88d10337581e1faabdcd5e8f144a
Israa Almahmoud <99071791@mydavinci.nl>
2022-10-10 15:10:40 +0200

    new opdracht "Dagen"

---
+++ leren.programmeren/module04-lijstjes.en.samenstellingen/collections/dagen.py
@@ -0,0 +1,34 @@
+
+weekdagen= ('maandag','dinsdag','woensdag','donderdag','vrijdag','zaterdag','zondag')
+print('Alle dagen van de week zijn:' , weekdagen[:7])
+print('De werkdagen zijn: ' , weekdagen [:5]) #tot 5 printen
+print ('De weekenddagen zijn: ' , weekdagen[-2:])
+# #  # :: == Geheel getal omkeren en het dan terug converteren naar een string
+print('\nAlle dagen van de week in omgekeerde volgorde zijn: ', weekdagen [:: -1])
+print('De werkdagen in omgekeerde volgorde zijn: ' , weekdagen [-3::-1])
+print('De weekenddagen in omgekeerde volgorde zijn: ' ,weekdagen [:4:-1]) #begint van 4
+
+
+
+
+
+
+
+
+
+
+#_____ andere manieren (onder elkaar en met for loop)
+
+# dagen = ("maandag", "dinsdag", "woensdag", "donderdag",
+#          "vrijdag", "zaterdag", "zondag")
+# for dag in dagen:
+#   print(dag)
+
+# # resultaat:
+# maandag
+# dinsdag
+# woensdag
+# donderdag
+# vrijdag
+# zaterdag
+# zondag
