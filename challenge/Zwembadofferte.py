d157813d926c32b4efd0d0858f6a8bd2c8f81c75
Israa Almahmoud <99071791@mydavinci.nl>
2022-10-13 14:28:41 +0200

    stap 1 van de challenge

--- challenge/Zwembadofferte.py
+++ challenge/Zwembadofferte.py
@@ -7,4 +7,3 @@ breedte= float(input('Voer aub de breedte van het zwembad in meters:  '))
 diepte= float(input('Voer aub de diepte van het zwembad in meters:  '))
 inhoud= lengte * breedte * diepte
 print (f'{inhoud} m3 grond in totaal moet afgevoerd worden.')
-
