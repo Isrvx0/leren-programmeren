eda992babc04abf5d61cd30fdf49f92278569254
Israa Almahmoud <99071791@mydavinci.nl>
2022-10-14 11:49:15 +0200

    opdracht M&M

--- leren.programmeren/module04-lijstjes.en.samenstellingen/collections/m&m.py
+++ leren.programmeren/module04-lijstjes.en.samenstellingen/collections/m&m.py
@@ -1,7 +1,18 @@
 lege_zak= []
-kleuren_lijst= ('oranje','blauw','groen','bruin')
 
-aantal= int (input ("Hoevee M&M's er aan de zak toegevoegd moeten worden?  "))
-lege_zak.append(aantal)
+print (f'Het zit nu 0 M&Ms er in de zak')
+kleuren_lijst= ('oranje','blauw','groen','bruin')
+hoeveelheid= int (input ("Hoeveel M&M's er aan de zak toegevoegd moeten worden?  "))
+delen= 4
+
+integer   = hoeveelheid/delen
+remainder = hoeveelheid%delen
+
+for i in range(delen):
+   lege_zak.append(integer)
+for i in range(remainder):
+   lege_zak[i]+=1
 zipped = zip (kleuren_lijst,lege_zak)
-print(list(zipped))
+
+print (list(zipped))
+print (f'Het zit nu {hoeveelheid} M&Ms er in de zak')
--- leren.programmeren/module04-lijstjes.en.samenstellingen/collections/tempCodeRunnerFile.py
+++ leren.programmeren/module04-lijstjes.en.samenstellingen/collections/tempCodeRunnerFile.py
@@ -1,1 +1,1 @@
-zaterd
+hoeveelheid= int (input ("Hoeveel M&M's er aan de zak toegevoegd moeten worden?  "))
