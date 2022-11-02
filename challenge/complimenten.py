1a9045645dc6995784d54626c40ebfa7d290d3bf
Israa Almahmoud <99071791@mydavinci.nl>
2022-11-02 10:21:28 +0100

    complimenten opdracht

---
+++ challenge/complimenten.py
@@ -0,0 +1,26 @@
+import random
+from random import randint
+
+# # complimenten= ['je bent geweldig' , 'je bent mooi' , 'je bent leuk' , 'je bent lief' , 'je bent perfect']
+# # naam= input('wat is je naam?  ')
+
+# # hoeveel= int(input('Hoeveel complimenten wil je?  '))
+
+# # for comp in range (hoeveel):
+# #     random_choice= random.choice(complimenten)
+# #     print(random_choice , naam)
+
+# #------uitbereiden
+
+naam= input('wat is je naam?  ')
+hoeveel= int(input('Hoeveel complimenten wil je?  '))
+complimenten= ['je bent geweldig' , 'je ziet mooi eruit' , 'je bent leuk' , 'je bent lief' , 'je bent special']
+oude_random= 0
+
+for compliment in range (hoeveel):
+    random_choice= random.choice(complimenten)
+    while random_choice == oude_random:
+         random_choice= random.choice(complimenten)
+    if random_choice != oude_random:
+        print (random_choice , naam)
+    oude_random = random_choice
