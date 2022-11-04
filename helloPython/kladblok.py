1f583d7b27e35b25fec87bf420faf511c7514148
Israa Almahmoud <99071791@mydavinci.nl>
2022-11-04 12:08:13 +0100

    lamda in python

--- helloPython/kladblok.py
+++ helloPython/kladblok.py
@@ -5,3 +5,12 @@ def tafelVanNummers (nummer):
         print (f'{nummer} x {i} = ', nummer*i)
 
 print(tafelVanNummers(nummerr))
+
+# ______
+
+nummerr= int(input('voer een nummer:  '))
+
+print (f'tafel van nummer {nummerr} : \n')
+tafel_van_nummers= lambda nummer : [print(f'{i} * {nummer} =', i*nummer) for i in range (1,11)]
+
+print(tafel_van_nummers(nummerr))
