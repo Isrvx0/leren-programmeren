8dfc2f9ec2958e8379aacee8cbe3a04231d041f4
Israa Almahmoud <99071791@mydavinci.nl>
2022-11-04 11:50:42 +0100

    def funcion

--- helloPython/kladblok.py
+++ helloPython/kladblok.py
@@ -1,22 +1,7 @@
-import random
+nummerr= int(input('voer een nummer:  '))
+def tafelVanNummers (nummer):
+    print (f'tafel van {nummer} is:')
+    for i in range (1,11):
+        print (f'{nummer} x {i} = ', nummer*i)
 
-kleuren_lijst=  ('rood','blauw','groen','geel','bruin') #keys
-aantal_MMs= int (input ("Hoeveel M&M's er aan de zak toegevoegd moeten worden?  ")) #values
-zak_MMs= {} 
-
-values= [] #voor het random delen van values
-for i in range (4):
-    random_number = random.randint(0,aantal_MMs)
-    values.append(random_number)
-    aantal_MMs-=random_number
-values.append(aantal_MMs)
-
-zipp_zak = zip(kleuren_lijst,values) #values en keys samen zippen
-zak_MMs = dict(zipp_zak) 
-
-
-for key in list(zak_MMs.keys()): # voor elke key in Dictionary 
-    if zak_MMs[key] ==0:  # als de waarde van de key gelijk aan 0
-        del zak_MMs[key] #wordt verwijdert uit de Dictionary
-
-print (zak_MMs)
+print(tafelVanNummers(nummerr))
