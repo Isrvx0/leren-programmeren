b1d2536397ee16f24287784bbcb7989ad235e268
Israa Almahmoud <99071791@mydavinci.nl>
2022-11-27 12:56:35 +0100

    opgave 8

--- challenge/oefenen-met-lists.py
+++ challenge/oefenen-met-lists.py
@@ -7,20 +7,27 @@ for nummer in range (1,14):
 
 #opdarcht 2 t/m 12:
 #opgave 2:
-getalen_lijst = ['5','12','19','27','3']
+getalen_lijst = [5,12,19,27,3]
 
 #opgave 3:
-getalen_lijst.append('25')
+getalen_lijst.append(25)
 
 #opgave 4:
 print(len(getalen_lijst))
 
 #opgave 5:
 print (getalen_lijst)
-getalen_lijst.remove('12')
+getalen_lijst.remove(12)
 print(getalen_lijst)
 
 #opgave 6:
 print (getalen_lijst)
 getalen_lijst.pop(0)
 print(getalen_lijst)
+
+#opgave 7:
+getalen_lijst.insert(0,36)
+print (getalen_lijst)
+
+#opgave 8:
+print(sum(getalen_lijst))
