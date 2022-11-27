8d3d435057c3ff960d89b8b451396d10863dff88
Israa Almahmoud <99071791@mydavinci.nl>
2022-11-27 12:31:40 +0100

    opgave 2

--- challenge/oefenen-met-lists.py
+++ challenge/oefenen-met-lists.py
@@ -4,3 +4,6 @@ for nummer in range (1,14):
     print (f'Tafel van {nummer}:')
     for multiplier in range (1,11):
         print(f'{nummer} x {multiplier} =  ' , nummer * multiplier)
+
+#opdarcht 2 t/m 12:
+getalen_lijst = ['5','12','19','27','3']
