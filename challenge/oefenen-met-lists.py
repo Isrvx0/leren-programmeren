8afe2b3f3ff59a56c4f378b3d05108afbce3efbd
Israa Almahmoud <99071791@mydavinci.nl>
2022-11-27 12:30:00 +0100

    opdarcht 1 van de challenge.

---
+++ challenge/oefenen-met-lists.py
@@ -0,0 +1,6 @@
+#opdracht 1.
+
+for nummer in range (1,14):
+    print (f'Tafel van {nummer}:')
+    for multiplier in range (1,11):
+        print(f'{nummer} x {multiplier} =  ' , nummer * multiplier)
