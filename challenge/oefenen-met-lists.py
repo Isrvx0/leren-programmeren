16487e48974997f006a8331c7df97eabd01c3c9b
Israa Almahmoud <99071791@mydavinci.nl>
2022-11-27 12:39:00 +0100

    opgave 5

--- challenge/oefenen-met-lists.py
+++ challenge/oefenen-met-lists.py
@@ -6,6 +6,17 @@ for nummer in range (1,14):
         print(f'{nummer} x {multiplier} =  ' , nummer * multiplier)
 
 #opdarcht 2 t/m 12:
+#opgave 2:
 getalen_lijst = ['5','12','19','27','3']
+
+#opgave 3:
 getalen_lijst.append('25')
+
+#opgave 4:
 print(len(getalen_lijst))
+
+#opgave 5:
+print (getalen_lijst)
+getalen_lijst.remove('12')
+print(getalen_lijst)
+
