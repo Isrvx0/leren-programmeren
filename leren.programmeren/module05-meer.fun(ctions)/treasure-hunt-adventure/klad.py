60cb874cacedad2293992559c6964c6053bed0bc
Israa Almahmoud <99071791@mydavinci.nl>
2022-12-18 13:34:14 +0100

    getFromListByKeyIs

--- leren.programmeren/module05-meer.fun(ctions)/treasure-hunt-adventure/klad.py
+++ leren.programmeren/module05-meer.fun(ctions)/treasure-hunt-adventure/klad.py
@@ -1,10 +1,95 @@
-from data import *
-from functions import copper2gold 
+from data import friends
+from functions import getFromListByKeyIs
 
-people = 10
-horses = 10
+list = [{
+    'name' : 'Jorick',
+    'shareWith' : True,
+    'adventuring' : True,
+    'cash' : {
+        'platinum' : 0,
+        'gold' : 9,
+        'silver' : 9,
+        'copper' : 43
+    }
+},{
+    'name' : 'Grommel',
+    'shareWith' : True,
+    'adventuring' : True,
+    'cash' : {
+        'platinum' : 1,
+        'gold' : 2,
+        'silver' : 2,
+        'copper' : 8
+    }
+},{
+    'name' : 'Tristan',
+    'shareWith' : False,
+    'adventuring' : True,
+    'cash' : {
+        'platinum' : 0,
+        'gold' : 2,
+        'silver' : 17,
+        'copper' : 11
+    }
+},{
+    'name' : 'Massimo',
+    'shareWith' : True,
+    'adventuring' : True,
+    'cash' : {
+        'platinum' : 0,
+        'gold' : 5,
+        'silver' : 9,
+        'copper' : 3
+    }
+},{
+    'name' : 'Durbane',
+    'shareWith' : True,
+    'adventuring' : False,
+    'cash' : {
+        'platinum' : 1,
+        'gold' : 5,
+        'silver' : 12,
+        'copper' : 11
+    }
+},{
+    'name' : 'Otho',
+    'shareWith' : True,
+    'adventuring' : True,
+    'cash' : {
+        'platinum' : 0,
+        'gold' : 1,
+        'silver' : 2,
+        'copper' : 7
+    }
+}]
 
-cost_people = people * copper2gold(COST_FOOD_HUMAN_COPPER_PER_DAY)
-coste_horses = horses * copper2gold(COST_FOOD_HORSE_COPPER_PER_DAY)
-totaal = round((cost_people + coste_horses) * JOURNEY_IN_DAYS , 2)
-print (totaal)
+# key = input('choose 1 :   ')
+# value = input('choose 2 :   ') 
+
+# def boolean(choice):
+#     if choice == "True":
+#         return True
+#     if choice == "False":
+#         return False
+
+
+
+# # for teller in range (0,len(friends)):
+# #     if choose_1 in friends[teller] == True:
+# #         print (friends[teller])
+ 
+
+# if value == 'True' or value == 'False':
+#     value = boolean(value)
+#     for teller in range (0,len(list)):
+#         if value == True:
+#             if list[teller][key]: 
+#                 print(list[teller]['name'])
+#         elif value == False:
+#             if not list[teller][key]: 
+#                 print(list[teller]['name'])
+# else:
+#     for teller in range (0,len(list)):
+#         if list[teller][key] and list[teller][value]:
+#             print(list[teller]['name'])       
+
