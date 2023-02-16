6581181ebf43a21abb565abab310739fa8abc517
Israa Almahmoud <99071791@mydavinci.nl>
2023-02-16 10:15:22 +0100

    pseudocode en flowchart van Ijs-salon

---
+++ leren.programmeren/module05-meer.fun(ctions)/een-dry-ijssalon/pseudocode_ijssalon.py
@@ -0,0 +1,35 @@
+# druk de welkom zin.
+
+
+# zolang je iets wilt bestellen:
+#   vragen over de hoeveelheid van het bolletjes die u wil.
+#   als de keuze op de vraag (van 1 t/m 3):
+#           vragen of  de aantal bolletjes in een hoorntje of een bakje?
+#           als de keuze bakje of hoorntje:
+#               vragen of ze nog meer willen bestellen?
+#               als de antwoord stop is:
+#                   drukt 'Bedankt en tot zien!'
+#                   en dan stopt de programma
+#               anders gaat wordt hij weer weer gevraagd
+#               en als hij geen van beide antwoorden geeft, wordt de vraag nog een keer gevraagd.
+
+
+#   als de keuze op vraag (van 4 t/m 8):
+#       bedrukt de programma 'je krijgt van ons een bakje met de gevraagde aantal bolletjes'
+#       vragen of  de aantal bolletjes in een hoorntje of een bakje?
+#       als de keuze bakje of hoorntje:
+#           vragen of ze nog meer willen bestellen?
+#           als de antwoord stop is:
+#               drukt 'Bedankt en tot zien!'
+#               en dan stopt de programma
+#           anders gaat wordt hij weer weer gevraagd
+#           en als hij geen van beide antwoorden geeft, wordt de vraag nog een keer gevraagd.   
+
+
+#   als de keuze op vraag grooter dan 8:
+#       bedrukt de programma 'sorry, zulke grote bakken hebben we niet'
+#       en wordt de vraag weer herhaald.
+
+
+#   andere keuzes: 
+#       bedrukt de programma 'sorry dat snap ik niet'
