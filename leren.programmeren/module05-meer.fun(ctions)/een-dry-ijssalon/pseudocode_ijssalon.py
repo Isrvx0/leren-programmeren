f6629e099e841b00381a60dc8f5d73fbc98f4287
Israa Almahmoud <99071791@mydavinci.nl>
2023-02-16 11:13:08 +0100

    verbetert

--- leren.programmeren/module05-meer.fun(ctions)/een-dry-ijssalon/pseudocode_ijssalon.py
+++ leren.programmeren/module05-meer.fun(ctions)/een-dry-ijssalon/pseudocode_ijssalon.py
@@ -2,27 +2,27 @@
 
 
 # zolang je iets wilt bestellen:
-#   vragen over de hoeveelheid van het bolletjes die u wil.
+#   vragen over de hoeveelheid van het bolletjes die u wilt.
 #   als de keuze op de vraag (van 1 t/m 3):
-#           vragen of  de aantal bolletjes in een hoorntje of een bakje?
+#           vragen of de aantal bolletjes in een hoorntje of een bakje wilt?
 #           als de keuze bakje of hoorntje:
 #               vragen of ze nog meer willen bestellen?
 #               als de antwoord stop is:
-#                   drukt 'Bedankt en tot zien!'
+#                   drukt de programma 'Bedankt en tot zien!'
 #                   en dan stopt de programma
-#               anders gaat wordt hij weer weer gevraagd
+#               anders wordt hij weer gevraagd
 #               en als hij geen van beide antwoorden geeft, wordt de vraag nog een keer gevraagd.
 
 
 #   als de keuze op vraag (van 4 t/m 8):
 #       bedrukt de programma 'je krijgt van ons een bakje met de gevraagde aantal bolletjes'
-#       vragen of  de aantal bolletjes in een hoorntje of een bakje?
+#       vragen of de aantal bolletjes in een hoorntje of een bakje?
 #       als de keuze bakje of hoorntje:
 #           vragen of ze nog meer willen bestellen?
 #           als de antwoord stop is:
-#               drukt 'Bedankt en tot zien!'
+#               drukt de programma 'Bedankt en tot zien!'
 #               en dan stopt de programma
-#           anders gaat wordt hij weer weer gevraagd
+#           anders wordt hij weer gevraagd
 #           en als hij geen van beide antwoorden geeft, wordt de vraag nog een keer gevraagd.   
 
 
@@ -32,4 +32,4 @@
 
 
 #   andere keuzes: 
-#       bedrukt de programma 'sorry dat snap ik niet'
+#       bedrukt de programma 'sorry, dat snap ik niet'
