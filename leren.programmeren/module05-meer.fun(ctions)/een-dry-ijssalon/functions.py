1d1ec71a81bc2852dd2336696515a3b0818797cf
Israa Almahmoud <99071791@mydavinci.nl>
2023-03-02 09:54:39 +0100

    code verbetert

--- leren.programmeren/module05-meer.fun(ctions)/een-dry-ijssalon/functions.py
+++ leren.programmeren/module05-meer.fun(ctions)/een-dry-ijssalon/functions.py
@@ -1,37 +1,38 @@
+import sys
+
 def start_function():
-    welkom_message = "Welkom bij Papi Gelato. Je mag alle smaken kiezen zolang het maar vanille ijs is"
+    welkom_message = "Welkom bij Papi Gelato. Je mag alle smaken kiezen zolang het maar vanille ijs is\n"
     return welkom_message
 
 def stap_1():
     try:
-        aantal_bolletjes = int(input("\nHoeveel bolletjes wilt u?   "))
+        aantal_bolletjes = int(input("Hoeveel bolletjes wilt u?   "))
         if aantal_bolletjes <= 3:
-            stap_2(aantal_bolletjes)
+            return stap_2(aantal_bolletjes)
         elif aantal_bolletjes >= 4 and aantal_bolletjes <= 8:
-            print(f"Dan krijgt u van mij een bakje met {aantal_bolletjes} bolletjes")
-            stap_3()
+            return stap_3(aantal_bolletjes)
         elif aantal_bolletjes > 8:
             print('Sorry, zulke grote bakken hebben we niet')
-            stap_1()
+            return stap_1()
     except ValueError:
         print('Sorry dat snap ik niet!' )
-        stap_1()
+        return stap_1()
     
 
 def stap_2(aantal):
-    keuze= input(f'\nWilt u deze {aantal} bolletjes in een hoorntje of een bakje?  ')
+    keuze= input(f'Wilt u deze {aantal} bolletjes in een hoorntje of een bakje?  ')
     if keuze.lower() in ('bakje' , 'hoorntje') :
-       print(f"Hier is uw {aantal} bolletjes in een {keuze}")
-       stap_3()
+       print(f"\nHier is uw {aantal} bolletjes in een {keuze}")
+       return stap_3()
     else:
         print('Sorry dat snap ik niet!' )
-        stap_2(aantal)
+        return stap_2(aantal)
 
-def stap_3():
-    extra = input('\nWilt u nog meer bestellen?  ')
+def stap_3(aantal):
+    extra = input(f'Dan krijgt u van mij een bakje met {aantal} bolletjes\nWilt u nog meer bestellen?  ')
     if extra.lower() in ("stop" , "nee"):
-        print("Bedankt en tot ziens! ")
-        exit()
+        sys.exit("Bedankt en tot ziens! ")
+
     else:
-        stap_1()
+        return stap_1()
 
