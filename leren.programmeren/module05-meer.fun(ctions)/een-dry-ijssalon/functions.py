7f7ac29ef088a37ae61760f7ea68079d4f601635
Israa Almahmoud <99071791@mydavinci.nl>
2023-03-10 12:48:49 +0100

    ijssalon programma verbetert

--- leren.programmeren/module05-meer.fun(ctions)/een-dry-ijssalon/functions.py
+++ leren.programmeren/module05-meer.fun(ctions)/een-dry-ijssalon/functions.py
@@ -2,35 +2,73 @@ def welkom_message():
     welkom_message = "Welkom bij Papi Gelato. Je mag alle smaken kiezen zolang het maar vanille ijs is\n"
     return welkom_message
 
-def vraag_naar_aantal(aantal_bolletjes):
-    try:
-        aantal_bolletjes = int(input("Hoeveel bolletjes wilt u?   "))
-        if aantal_bolletjes <= 3:
-            return_variable = keuze_maken(aantal_bolletjes)
-        elif aantal_bolletjes >= 4 and aantal_bolletjes <= 8:
-            return_variable =  meer_bestellen(aantal_bolletjes)
-        elif aantal_bolletjes > 8:
-            print('Sorry, zulke grote bakken hebben we niet')
-            return_variable =  vraag_naar_aantal()
-    except ValueError:
-        print('Sorry dat snap ik niet!' )
-        return_variable =  vraag_naar_aantal()
-    return return_variable
+
+def hoeveelheid_vragen():
+    aantal_bolletjes = " " 
+    while type(aantal_bolletjes) != int:
+        try:
+            aantal_bolletjes = int(input("Hoeveel bolletjes wilt u?"))
+        except ValueError:
+            print('Sorry dat snap ik niet!' )
+    return aantal_bolletjes
 
 def keuze_maken(aantal):
-    keuze= input(f'Wilt u deze {aantal} bolletjes in een hoorntje of een bakje?  ')
-    if keuze.lower() in ('bakje' , 'hoorntje') :
-       return_variable = meer_bestellen(aantal)
-    else:
-        print('Sorry dat snap ik niet!' )
-        return_variable =  keuze_maken(aantal)
-    return return_variable 
+    choice = True
+    while choice:
+        keuze = input(f'Wilt u deze {aantal} bolletjes in een hoorntje of een bakje?  ')
+        if  keuze.lower() in ("hoorntje" , "bakje"):
+            print(f"Dan krijgt u van mij een {keuze} met {aantal} bolletjes\n")
+            choice = False
+        else:
+            print('Sorry dat snap ik niet!' )
+    return keuze
 
-def meer_bestellen(aantal):
-    extra = input(f'Dan krijgt u van mij een bakje met {aantal} bolletjes\nWilt u nog meer bestellen?  ')
-    if extra.lower() in ("stop" , "nee"):
-        print("Bedankt en tot ziens! ")
-        exit()
-    else:
-        return_variable = vraag_naar_aantal()
-    return return_variable 
+def buy_more():
+    extra = input('Wilt u nog meer bestellen?  ')
+    return extra 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def vraag_naar_aantal(aantal_bolletjes):
+#     try:
+#         aantal_bolletjes = int(input("Hoeveel bolletjes wilt u?   "))
+#         if aantal_bolletjes <= 3:
+#             return_variable = keuze_maken(aantal_bolletjes)
+#         elif aantal_bolletjes >= 4 and aantal_bolletjes <= 8:
+#             return_variable =  meer_bestellen(aantal_bolletjes)
+#         elif aantal_bolletjes > 8:
+#             print('Sorry, zulke grote bakken hebben we niet')
+#             return_variable =  vraag_naar_aantal()
+#     except ValueError:
+#         print('Sorry dat snap ik niet!' )
+#         return_variable =  vraag_naar_aantal()
+#     return return_variable
+
+# def keuze_maken(aantal):
+#     keuze= input(f'Wilt u deze {aantal} bolletjes in een hoorntje of een bakje?  ')
+#     if keuze.lower() in ('bakje' , 'hoorntje') :
+#        return_variable = meer_bestellen(aantal)
+#     else:
+#         print('Sorry dat snap ik niet!' )
+#         return_variable =  keuze_maken(aantal)
+#     return return_variable 
+
+# def meer_bestellen(aantal):
+#     extra = input(f'Dan krijgt u van mij een bakje met {aantal} bolletjes\nWilt u nog meer bestellen?  ')
+#     if extra.lower() in ("stop" , "nee"):
+#         print("Bedankt en tot ziens! ")
+#         exit()
+#     else:
+#         return_variable = vraag_naar_aantal()
+#     return return_variable 
--- leren.programmeren/module05-meer.fun(ctions)/een-dry-ijssalon/ijs_salon.py
+++ leren.programmeren/module05-meer.fun(ctions)/een-dry-ijssalon/ijs_salon.py
@@ -1,4 +1,21 @@
 from functions import *
 
 print(welkom_message())
-print(vraag_naar_aantal())
+
+extra_bestellen = True
+while extra_bestellen:
+    aantal_bolletjes = hoeveelheid_vragen()
+    if aantal_bolletjes <= 8:
+        if aantal_bolletjes <= 3:
+            keuze = keuze_maken(aantal_bolletjes)
+        else:
+            print(f"Dan krijgt u van mij een bakje met {aantal_bolletjes} bolletjes\n")
+        
+        meer_bestellen = buy_more()
+        if meer_bestellen.lower() in ( "stop" , "nee"):
+            print("Bedankt en tot ziens! ")
+            extra_bestellen = False
+    elif aantal_bolletjes > 8:
+        print('Sorry, zulke grote bakken hebben we niet')
+    else:
+        print('Sorry dat snap ik niet!' )
