7d9fc69fd7e5141a87e7cafcbf30a98e8e196976
Israa Almahmoud <99071791@mydavinci.nl>
2023-04-20 11:41:55 +0200

    avi score in de pragram.py

--- challenge_opd/assessment/avi_score/AVI/program.py
+++ challenge_opd/assessment/avi_score/AVI/program.py
@@ -9,6 +9,8 @@ def calculate():
     characterLabel.config(text=f"Characters: {getNumberOfCharacters(textToBeCalculated)}")
     sentencesLabel.config(text =f"Zinnen: {getNumberOfSentences(textToBeCalculated)}")
     wordsLabel.config(text =f"Woorden: {getNumberOfWords(textToBeCalculated)}")
+    aviScoreLabel.config(text =f"AVI score: {aviScore(textToBeCalculated)}")
+
 
 
 #variables TK
@@ -17,15 +19,19 @@ root.title('Text analyser') # set title
 root.geometry('600x600')    # set dimension
 calculateInput = tk.Text(root, width = 70, height = 30, background='lightgrey')            # generate imput element
 calculateButton = ttk.Button(root, text='Bereken score(s)', command=calculate)        # generate button when pressed -> calculate
+
 characterLabel = tk.Label(root, text=f'Karakters:', width=20, bg='black', fg='white') # generate characterLabel
 sentencesLabel = tk.Label(root, text=f'Zinnen:', width=20, bg='black', fg='white')    # generate sentencesLabel
 wordsLabel = tk.Label(root, text=f'Woorden:', width=20, bg='black', fg='white')    # generate wordsLabel
+aviScoreLabel = tk.Label(root, text=f'AVI score:', width=20, bg='black', fg='white')    # generate aviScoreLabel
+
 
 calculateInput.place(x=20, y=20)   # place is one of the ways to put elements on root (window).
 calculateButton.place(x=20, y=520)
 characterLabel.place(x=180, y=560)
 sentencesLabel.place(x=20, y=560)
 wordsLabel.place(x=345, y=560)
+aviScoreLabel.place(x=180, y=520)
 
 # start program
 root.mainloop() # runs until stopped with default stop button.
